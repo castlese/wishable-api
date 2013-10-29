b875938d27efcb482964238d5bc1df8ab591e55d
Brian Kenny <brian@bkenny.com>
2013-10-29 10:57:24 +0000

    Return auth token and show response on documentation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	def create
 		user = User.new(params[:user])
 		if user.save
-			response = {:success => true, :user => user.as_json}
+			user.ensure_authentication_token!
+
+			response = {:success => true, :user => user.as_json(:only => [:id, :email, :authentication_token, :created_at, :updated_at])}
 			render :json => response, :status => 200
 		else
 			response = {:success => false, :errors => user.errors}
