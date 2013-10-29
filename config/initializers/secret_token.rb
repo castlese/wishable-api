1be85aa17a7d8d96bbfcc871d3ed3f6541e242cc
Brian Kenny <brian@bkenny.com>
2013-10-29 08:35:02 +0000

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishableApi::Application.config.secret_token = '3aafaedc7cef538cee10bc70655e14672f26f911c5b9ef78de9aab402fdd1fc700047bf7bc868576ca3b5694a96134c1a1079bba764336b51c8e71e84525197d'
